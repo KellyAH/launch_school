0119e6b1c18c8c39d1f4735e738650f21fd05261
Kelly Hong <kellyhong@telmate.com>
2019-01-24 17:42:06 -0800

    update ignore file edit ex 01 and start ex 02

---
+++ book_intro_to_prog_with_ruby/ch_01_the_basics/02_find_digit_to_tens.rb
@@ -0,0 +1,12 @@
+=begin
+  Use the modulo operator, division, or a combination of both to take a 4 digit number
+  and find the digit in the: 1) thousands place 2) hundreds place 3) tens place 4) ones place
+=end
+
+FOUR_DIGIT_NUMBER = 5328
+
+puts "#{FOUR_DIGIT_NUMBER} to the thousands place is #{FOUR_DIGIT_NUMBER/1000}"
+puts "#{FOUR_DIGIT_NUMBER} to the hundreds place is #{FOUR_DIGIT_NUMBER/100}"
+puts "#{FOUR_DIGIT_NUMBER} to the tens place is #{FOUR_DIGIT_NUMBER/10}"
+puts "#{FOUR_DIGIT_NUMBER} to the ones place is #{FOUR_DIGIT_NUMBER/1}"
+
