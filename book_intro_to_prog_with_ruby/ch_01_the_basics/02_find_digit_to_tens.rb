00d697baada74efcc52b16b537f6500665f9aecf
Kelly Hong <k_ahong@yahoo.com>
2020-05-25 18:45:11 -0700

    redo tens place exercise 2

--- book_intro_to_prog_with_ruby/ch_01_the_basics/02_find_digit_to_tens.rb
+++ book_intro_to_prog_with_ruby/ch_01_the_basics/02_find_digit_to_tens.rb
@@ -1,13 +1,41 @@
-=begin
-  Use the modulo operator, division, or a combination of both to take a 4 digit number
-  and find the digit in the: 1) thousands place 2) hundreds place 3) tens place 4) ones place
-=end
+# URL: https://launchschool.com/books/ruby/read/basics
+#
+# Use the modulo operator, division, or a combination of both to take a 4 digit number
+# and find the digit in the: 1) thousands place 2) hundreds place 3) tens place 4) ones place
 
-FOUR_DIGIT_NUMBER = 5328
+# NOTES:
+#
+# INPUT: 4 digit number, E.g. 4321
+#
+# OUTPUT:
+# E.g.
+# Digit in the thousands place: 4
+# Digit in the hundreds place: 3
+# Digit in the tens place: 2
+# Digit in the ones place: 1
 
-puts "The 4 digit number is: #{FOUR_DIGIT_NUMBER}"
-puts
-puts "The digit in the thousands place is #{FOUR_DIGIT_NUMBER/1000}"
-puts "The digit in the hundreds place is #{FOUR_DIGIT_NUMBER%1000/100}"
-puts "The digit in the tens place is #{FOUR_DIGIT_NUMBER%100/10}"
-puts "The digit in the ones place is #{FOUR_DIGIT_NUMBER%10}"
+def captured_number
+  puts "Enter a 4 digit number and press ENTER:"
+  gets.chomp.to_i
+end
+
+starting_number = captured_number
+
+while starting_number.negative? || starting_number.digits.count > 4 || starting_number.digits.count < 4
+  starting_number = captured_number
+  # NOTE: I did not use starting_number.digits.count to determine integer length
+  # because it cannot handle negative numbers
+  break if starting_number.to_s.split('').size == 4
+end
+
+thousands = starting_number/1000
+hundreds = starting_number%1000/100
+tens = starting_number%1000%100/10
+ones = starting_number%1000%10
+
+puts "the number in the thousands place is: #{thousands}"
+puts "the number in the hundreds place is: #{hundreds}"
+puts "the number in the tens place is: #{tens}"
+puts "the number in the ones place is: #{ones}"
+
+
