ccbdfb5f8a3b70e458591f97f6b607176327de91
Kelly Hong <k_ahong@yahoo.com>
2020-05-30 21:08:36 -0700

    redo exercise 4 array of years

--- book_intro_to_prog_with_ruby/ch_01_the_basics/04_array_movie_titles.rb
+++ book_intro_to_prog_with_ruby/ch_01_the_basics/04_array_movie_titles.rb
@@ -1,18 +1,30 @@
-=begin
-  Use the dates from the previous example and store them in an array.
-  Then make your program output the same thing as exercise 3.
-=end
+# URL: https://launchschool.com/books/ruby/read/basics
+#  Use the dates from the previous example and store them in an array.
+#  Then make your program output the same thing as exercise 3.
 
+years = [2015, 2021, 2020, 2002, 2011, 2013]
 
-movie_release_year =
-{
-    iron_man: 2008,
-    thor: 2011,
-    captain_america: 2011,
-    the_avengers: 2012,
-    ant_man: 2015
-}
+puts '-' * 10
+puts 'using direct reference via index'
+puts years[0]
+puts years[1]
+puts years[2]
+puts years[3]
+puts years[4]
+puts years[5]
 
-movie_years_array = puts movie_release_year.values
+puts '-' * 10
+puts 'using puts'
+puts years
 
-puts movie_years_array
+puts '-' * 10
+puts 'using each method'
+years.each {|year| puts year}
+
+puts '-' * 10
+puts 'using each_index method'
+years.each_index{|index| puts years[index]}
+
+puts '-' * 10
+puts 'using values_at method'
+puts years.values_at(0,1, 2, 3, 4, 5)
