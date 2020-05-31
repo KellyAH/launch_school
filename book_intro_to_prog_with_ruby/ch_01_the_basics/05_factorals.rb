62f566ffe5df0d40ea81340f9bbe2602bfc8a7e4
Kelly Hong <k_ahong@yahoo.com>
2020-05-30 22:18:12 -0700

    redo ex 5 with and without recursion

--- book_intro_to_prog_with_ruby/ch_01_the_basics/05_factorals.rb
+++ book_intro_to_prog_with_ruby/ch_01_the_basics/05_factorals.rb
@@ -1,43 +1,45 @@
+# URL: https://launchschool.com/books/ruby/read/basics
 # Write a program that outputs the factorial of the numbers 5, 6, 7, and 8.
 
-# debugging tracing to see what the method is doing
-# I NEEDED to expose all these values JUST TO SEE what the heck was happening in the loop .
-# so I could see why my code was returning or multiplying wrong values
+# A factoral is:
+# Factorials are products of every whole number from 1 to n.
+# For example, if n is 3 then 3! is 3 x 2 x 1 = 6.
 
-# def factorial(number)
-#   counter = 1
-#   product = number
-#   puts "product before loop: #{product}"
-#
-#   loop do
-#     puts "product at start of loop: #{product}"
-#     puts "counter before decrement: #{counter}"
-#     puts "calculation: #{product} * #{number - counter}"
-#
-#     product = product * (number - counter)
-#     puts "product after reassignment: #{product}"
-#
-#     counter += 1
-#     puts "counter after decrement: #{counter}"
-#     puts "-" * 8
-#
-#     break if counter >= number
-#
-#   end
-#
-#   return product
-# end
+puts '-' * 10
+puts 'not using recursion'
 
-def factorial(number)
-  counter = 1
-  product = number
-
-  loop do
-    product = product * (number - counter)
-    counter += 1
-    break if counter >= number
+# returns an array of the list of factorial numbers for a given number
+def factorial_numbers(number)
+  factorials = [number]
+  number.times do
+    if number > 1  # <-- ensures 0 is not pushed into the factorials array
+      factorials << number - 1
+      number -= 1
+    end
   end
-  return product
+  factorials
+end
+
+def factorial_non_recursion(numbers)
+  numbers.each do |number|
+    result = 1  # <-- reset result to 1 so factorial results from other numbers aren't combined together
+    factorials = factorial_numbers(number)
+    factorials.each {|number| result *= number}
+    puts "The factorial of the the number #{number} is #{result}"
+  end
+end
+
+numbers = [5, 6, 7, 8]
+factorial_non_recursion(numbers)
+
+puts '-' * 10
+puts 'using recursion'
+def factorial(number)
+  if number == 1  # <-- BASE CASE: scenario that tells recusion when to stop
+    return 1
+  else
+    number * factorial(number - 1)  # <-- RULE: determines when recursion should continue
+  end
 end
 
 puts "The factorial of the the number 5 is #{factorial(5)}"
