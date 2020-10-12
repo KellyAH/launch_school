fc353d505836986c767e3209e4d58cfee56b2c1d
Kelly Hong <k_ahong@yahoo.com>
2020-10-11 22:22:42 -0700

    redo ch 4 ex 2 and 3

--- book_intro_to_prog_with_ruby/ch_04_flow_control/02_hello_world.rb
+++ book_intro_to_prog_with_ruby/ch_04_flow_control/02_hello_world.rb
@@ -1,7 +1,11 @@
-=begin
-Write a method that takes a string as argument. The method should return a new, all-caps version of the string, only if the string is longer than 10 characters.
-Example: change "hello world" to "HELLO WORLD". (Hint: Ruby's String class has a few methods that would be helpful. Check the Ruby Docs!)
-=end
+# URL: https://launchschool.com/books/ruby/read/flow_control
+#
+# Write a method that takes a string as argument.
+# The method should return a new, all-caps version of the string,
+# only if the string is longer than 10 characters.
+#
+# Example: change "hello world" to "HELLO WORLD".
+# (Hint: Ruby's String class has a few methods that would be helpful. Check the Ruby Docs!)
 
 def uppercase_format(string)
   string.length > 10 ? string.upcase : string
@@ -10,3 +14,62 @@ end
 puts uppercase_format("hello world")
 puts uppercase_format("hello")
 puts uppercase_format("aatenChars")
+
+# ------------- #
+p "---- USE CASE STATEMENT ---"
+def uppercase_format_v02(string)
+  boolean = string.length > 10
+
+  case boolean
+  when true
+    string.upcase
+  when false
+    string
+  else
+    "the string was: #{string}"
+  end
+end
+
+puts uppercase_format_v02("hello world")
+puts uppercase_format_v02("hello")
+puts uppercase_format_v02("aatenChars")
+
+# ------------- #
+p "---- USE IF STATEMENT ---"
+def uppercase_format_v03(string)
+  if string.length > 10
+    string.upcase
+  else
+    string
+  end
+end
+
+puts uppercase_format_v03("hello world")
+puts uppercase_format_v03("hello")
+puts uppercase_format_v03("aatenChars")
+
+# ------------- #
+p "---- USE UNLESS STATEMENT ---"
+def uppercase_format_v04(string)
+  unless string.length > 10
+    string
+  else
+    string.upcase
+  end
+end
+
+puts uppercase_format_v04("hello world")
+puts uppercase_format_v04("hello")
+puts uppercase_format_v04("aatenChars")
+
+
+# ------------- #
+# expand on exercise
+p "---- USE DYNAMIC NUMBER ---"
+def dynamic_uppercase_format(string, number)
+  string.length > number ? string.upcase : string
+end
+
+puts dynamic_uppercase_format("hello world", 5)
+puts dynamic_uppercase_format("hello", 10)
+puts dynamic_uppercase_format("aatenChars", 0)
--- book_intro_to_prog_with_ruby/ch_04_flow_control/03_number.rb
+++ book_intro_to_prog_with_ruby/ch_04_flow_control/03_number.rb
@@ -1,25 +1,38 @@
-=begin
-  Write a program that takes a number from the user between 0 and 100
-  and reports back whether the number is between 0 and 50, 51 and 100, or above 100.
-=end
+# URL: https://launchschool.com/books/ruby/read/flow_control
+#
+# Write a program that takes a number from the user between 0 and 100
+# and reports back whether the number is between 0 and 50, 51 and 100, or above 100.
 
-def fetch_number
-  puts "Please enter a number between 0 and 100:"
+def prompt_for_user_input
+  puts "Please enter a number between 0 and 100"
   gets.chomp.to_i
 end
 
-def evaluate_number
-  number = fetch_number
+def valid_user_input?(number)
+  number.negative? == false && number <= 100 ? true : false
+end
 
-  if number < 0
-    "#{number} is a negative number!"
-  elsif number <= 50
-    "#{number} is between 0 and 50."
+def evaluate_user_input
+  number = prompt_for_user_input
+  while valid_user_input?(number) == false
+    number = prompt_for_user_input
+  end
+  number
+end
+
+def condition_check(number)
+  if number <= 50
+    p "#{number} is between 0 and 50."
   elsif number <= 100
-    "#{number} is between 51 and 100"
-  else number < 100
-    "#{number} is greater than 100"
+    p "#{number} is between 51 and 100"
+  else number > 100
+    p "#{number} is greater than 100"
   end
 end
 
-puts evaluate_number
+# finish exercise
+condition_check(evaluate_user_input)
+
+# expand on exercise by randomly generating the number
+number = rand(101)
+condition_check(number)
