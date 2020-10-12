f603b9030c24198c36e5d8e479bb4c6b3731ce27
Kelly Hong <k_ahong@yahoo.com>
2020-10-11 20:26:12 -0700

    redo ch 3 exercises

--- book_intro_to_prog_with_ruby/ch_03_methods/05_scream.rb
+++ book_intro_to_prog_with_ruby/ch_03_methods/05_scream.rb
@@ -1,14 +1,23 @@
-# Edit the method definition in exercise #4 so that it does print words on the screen. 2) What does it return now?
+# URL: https://launchschool.com/books/ruby/read/methods
+#
+# Edit the method definition in exercise #4
+# so that it does print words on the screen.
+# 2) What does it return now?
+#
+# def scream(words)
+#   words = words + "!!!!"
+#   return
+#   puts words
+# end
+#
+# scream("Yippeee")
+
 
 def scream(words)
   words = words + "!!!!"
   puts words
 end
 
-# refactored for brevity
-def modified_scream(words)
-  puts words + "!!!!"
-end
-
 scream("Yippeee")
-modified_scream("Yippeee")
+# => nil
+# because puts returns nil
