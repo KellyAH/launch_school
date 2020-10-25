3a56670dbbf54510e193b48dced8cf2e4b8ab430
Kelly Hong <k_ahong@yahoo.com>
2020-10-24 17:02:44 -0700

    redo ch 4 and 5 exercises

--- book_intro_to_prog_with_ruby/ch_04_flow_control/03_number_evaluator.rb
+++ book_intro_to_prog_with_ruby/ch_04_flow_control/03_number_evaluator.rb
@@ -12,8 +12,7 @@ def valid_user_input?(number)
   number.negative? == false && number <= 100 ? true : false
 end
 
-def evaluate_user_input
-  number = prompt_for_user_input
+def evaluate_user_input(number=prompt_for_user_input)
   while valid_user_input?(number) == false
     number = prompt_for_user_input
   end
@@ -22,17 +21,28 @@ end
 
 def condition_check(number)
   if number <= 50
-    p "#{number} is between 0 and 50."
+    "#{number} is between 0 and 50."
   elsif number <= 100
-    p "#{number} is between 51 and 100"
+    "#{number} is between 51 and 100"
   else number > 100
-    p "#{number} is greater than 100"
+    "#{number} is greater than 100"
   end
 end
 
 # finish exercise
-condition_check(evaluate_user_input)
+p condition_check(evaluate_user_input)
 
 # expand on exercise by randomly generating the number
 number = rand(101)
-condition_check(number)
+p condition_check(number)
+
+# solve exercise using case statement
+def condition_check_v02(number)
+  case
+  when number <= 50 then "#{number} is between 0 and 50."
+  when number <= 100 then "#{number} is between 51 and 100"
+  else "#{number} is greater than 100"
+  end
+end
+
+p condition_check_v02(evaluate_user_input)
