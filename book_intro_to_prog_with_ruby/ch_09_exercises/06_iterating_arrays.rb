e08c70253d7933d9d4bb5af59d531d19e6a2861c
Kelly Hong <k_ahong@yahoo.com>
2021-05-08 13:50:01 -0700

    do some hash exercies

--- book_intro_to_prog_with_ruby/ch_09_exercises/06_iterating_arrays.rb
+++ book_intro_to_prog_with_ruby/ch_09_exercises/06_iterating_arrays.rb
@@ -3,11 +3,22 @@ numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 # Exercise 4. Appended 11 to the end of the original array. Prepended 0 to the beginning.
 numbers.prepend(0)
 numbers.append(11)
-p numbers
+# p numbers
 
 # Exercise 5 deleted 11 And append a 3.
 numbers.delete(11)
 numbers.append(3)
+p "before mutation"
 p numbers
 
 # 6. Get rid of duplicates without specifically removing any one value.
+# mutate the caller
+result = numbers.uniq!
+
+# non mutating
+# result = numbers.uniq
+p "result: #{result}"
+
+p "after mutation"
+p "original array: #{numbers}"
+
---
+++ book_intro_to_prog_with_ruby/ch_09_exercises/08_iterating_hashes.rb
@@ -0,0 +1,47 @@
+# 8. Create a Hash, with one key-value pair, using both Ruby syntax styles.
+
+hash_one = {'a' => 1, 'b' => 2 }
+hash_two = { :a => 1, :b => 2 }
+
+# this syntax only works for keys that are symbols!
+hash_three =  { a: 1, b: 2 }
+
+## NOT PASSING A DEFAULT KEY VALUE ##
+hash_four = Hash.new
+hash_four[:a] = 1
+hash_four[:b] = 2
+# if no obj passed into Hash.new, then a default value is not set
+# for keys that don't exist
+puts hash_four[:c]
+puts hash_four[:c].class
+
+## PASSING A DEFAULT KEY VALUE ##
+# If obj is specified, this single object will be used for all default values.
+hash_five = Hash.new("default value")
+hash_five[:a] = 1
+hash_five[:b]
+hash_five["c"]
+puts hash_five
+puts hash_five[:b]
+puts hash_five["c"]
+
+#TODO why can't I do this?
+# puts { a: 1, b: 2 }
+# print { a: 1, b: 2 }
+
+puts hash_one
+puts hash_one
+puts hash_two
+puts hash_three
+puts hash_four
+
+## PASSING A BLOCK TO GENERATE HASH KEY VALUES ON THE FLY ##
+# TODO in what scenerio would I ever need to do this?
+# If a block is specified, it will be called with the hash object and the key, and should return the default value.
+# It is the block’s responsibility to store the value in the hash if required.
+
+hash_six = Hash.new {|hash, key| hash[key] = "the puppy is #{key}"}
+hash_six["happy"]
+hash_six["sad"]
+hash_six["asleep"]
+puts hash_six
