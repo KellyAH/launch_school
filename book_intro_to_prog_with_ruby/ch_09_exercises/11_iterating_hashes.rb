73b43577234946b48518dd6f01ab91f4e0bd2854
Kelly Hong <k_ahong@yahoo.com>
2021-05-12 16:20:00 -0700

    add more exercise files

---
+++ book_intro_to_prog_with_ruby/ch_09_exercises/11_iterating_hashes.rb
@@ -0,0 +1,4 @@
+# URL: https://launchschool.com/books/ruby/read/intro_exercises
+#
+# 11. Given the following data structures.
+# Write a program that copies the information from the array into the empty hash that applies to the correct person.
