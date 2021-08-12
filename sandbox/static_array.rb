1dda9aa809dc0b46dd6c42c912cc981616a48aea
Kelly Hong <k_ahong@yahoo.com>
2021-08-11 17:54:53 -0700

    build static array

--- sandbox/static_array.rb
+++ sandbox/static_array.rb
@@ -1,15 +1,18 @@
+require 'rspec'
+
 # This class just dumbs down a regular Array to be staticly sized.
 class StaticArray
+  # creates array of fixed length
   def initialize(length)
     self.store = Array.new(length, nil)
   end
 
-  # O(1)
+  # reading index
   def [](index)
     self.store[index]
   end
 
-  # O(1)
+  # saving value to index
   def []=(index, value)
     self.store[index] = value
   end
@@ -18,36 +21,67 @@ class StaticArray
   attr_accessor :store
 end
 
-a = StaticArray.new(2)
+class DynamicArray
+  def initialize
+    @length = 0
+    @array = StaticArray.new(length=5)
+  end
+
+  # [nil, nil]
+
+  # num of eles, NOT storage space
+  def length
+    @length
+  end
+
+  def [](index)
+    # DA length, index to fetch element from
+    # length = 4
+    #  0  1  2  3
+    # [n, n, n, n]
+    # [nil, nil, nil, 1]
+    #
+    # l = 4 and i = 4 == out of bounds
+    # l = 4 and i = 2 == out of bounds
+    raise "index out of bounds" if index >= self.length
+  end
+
+  # [nil, nil, nil, nil, 5]
+  # [x, x, x] <--
+  def push(value)
+    @array[@length] = value
+    @length = @length + 1
+  end
+
+end
+
+  # num of eles in array is not the same as space allocated
+  # [nil,nil,nil]  length = 0
+
+# use these tests https://github.com/matthewjf/algorithms/blob/master/dynamic_array_ring_buffer/spec/dynamic_array_spec.rb
+
+describe DynamicArray do
+  it "starts out empty" do
+    arr = DynamicArray.new
+    expect(arr.length).to eq(0)
+    expect do
+      arr[0]
+      end.to raise_error("index out of bounds")
+    end
+
+  it "pushes/pops items" do
+    arr = DynamicArray.new
+    5.times { |i| arr.push(i) }
+
+    # expect(arr.length).to eq(5)
+    # 5.times { |i| expect(arr[i]) == i }
+    #
+    # 4.downto(0) do |i|
+    #   expect(arr.pop).to eq(i)
+    # end
+    # expect(arr.length).to eq(0)
+  end
 
 
-p a.inspect
 
-a[0]="a"
-a[1]="b"
-a[2]="c"
-a[3]="d"
-
-p a.inspect
-
-# how dybnmic arrays work
-# avg constant time for R/W, pop push
-# amertize the spikes of time spent creating new array
-# R/W 1ms
-#
-# TODO: ring buffer
-# TODO: build dynamic array class that does below behavior usning static array class
-# TODO:
-#
-original_array = [1, 2, 3, 4, 5] # n
-
-# "the concept/idea" vs "the implementation"
-# abstract data type (defined requiements/behavior of data structure .e.g read/write to index) v.s. data structure (implementatin and usage of abstract data type)
-
-# 12ms - ceate new array and write each item ot it and the new item
-new_array =      [1, 2, 3, 4, 5, nil, nil, nil, nil, nil] # 2n
-# cost n operations / next n insertions
-
-original_array = [1, 2, 3, nil, nil, nil, nil, nil, nil, nil] # n = 10
-# pop
-new_array = [1, 2, nil, nil, nil] # n = 5
+end
