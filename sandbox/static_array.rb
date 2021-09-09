4c2b2c6b30e02f1ad1189a3e0fb069723989b240
Kelly Hong <k_ahong@yahoo.com>
2021-09-08 19:09:33 -0700

    dynamically grow the array

--- sandbox/static_array.rb
+++ sandbox/static_array.rb
@@ -1,5 +1,7 @@
 require 'rspec'
-
+# TODO: FUTURE: make a linked list array, alt method to grow array
+# TODO: study ruby hash map
+# TODO: ring buffer
 # This class just dumbs down a regular Array to be staticly sized.
 class StaticArray
   # creates array of fixed capacity
@@ -16,7 +18,7 @@ class StaticArray
   # saving value to index
   def []=(index, value)
     # throw error if setting array beyond the arrays capacity
-    raise Error "Out of bounds" if index >= @capacity
+    raise StandardError.new "Out of bounds" if index >= @capacity
     self.store[index] = value
   end
 
@@ -25,45 +27,40 @@ class StaticArray
 end
 
 class DynamicArray
-  def initialize
+  def initialize(initial_capacity=5)
+    @current_capacity = initial_capacity
     @length = 0
-    @array = StaticArray.new(length=5)
+    @array = StaticArray.new(length=initial_capacity)
   end
 
-  # [nil, nil]
-
-  # num of eles, NOT storage space
+  # number of elements, NOT the number storage space
   def length
     @length
   end
 
-  # todo fix this
-  def grow_array
-    # double size of array
-  end
-
+  # length = Dynamic array length
+  # index = index to fetch an element from
   def [](index)
-    # DA length, index to fetch element from
-    # length = 4
-    #  0  1  2  3
-    # [n, n, n, n]
-    # [nil, nil, nil, 1]
-    #
-    # l = 4 and i = 4 == out of bounds
-    # l = 4 and i = 2 == out of bounds
+
+    # if l = 4 and i = 4 == out of bounds
+    # if l = 4 and i = 2 == out of bounds
     raise "index out of bounds" if index >= self.length
+    @array[index]
   end
 
   # [nil, nil, nil, nil, 5]
   # [x, x, x] <-- pushes items from left to right.
   # because if it pushed right to left, array[0] would return nil and not the value you want.
   def push(value)
-    # TODO # if at capacity then GROW ARRAY
+    if length >= @current_capacity
+      grow
+    end
+
     @array[@length] = value
     @length = @length + 1
   end
 
-  # todo make pop take a arg
+  # TODO make pop take a arg
   # mutates caller and removes and returns last item
   def pop()
     # [0, 1, 2, 3, 4]
@@ -75,13 +72,29 @@ class DynamicArray
     @length = @length - 1
     result
   end
+
+  private
+  # double array size when array capacity is exceeded
+  def grow
+    # create a new array that is double sized
+    doubled_capacity = @current_capacity*2
+    new_array = StaticArray.new(length=doubled_capacity)
+
+    # populate new array
+    @current_capacity.times do |index|
+      new_array[index] = @array[index]
+    end
+
+    @current_capacity = doubled_capacity
+    @array = new_array
+  end
 end
 
   # num of eles in array is not the same as space allocated
   # [nil,nil,nil]  length = 0
 
 # uses these tests https://github.com/matthewjf/algorithms/blob/master/dynamic_array_ring_buffer/spec/dynamic_array_spec.rb
-# run via: rspec sandbox/static_array.rb --format doc
+# run tests: rspec sandbox/static_array.rb --format doc
 
 describe DynamicArray do
   it "starts out empty" do
@@ -105,15 +118,20 @@ describe DynamicArray do
     expect(arr.length).to eq(0)
   end
 
-  it "breaks on array larger than 5" do
-    arr = DynamicArray.new
+  # it "throws Error on array larger than 5" do
+  #   arr = DynamicArray.new
+  #   expect {
+  #     6.times { |i| arr.push(i) }
+  #   }.to raise_error(StandardError)
+  # end
 
-    6.times { |i| arr.push(i) }
-    expect(arr.length).to eq(6)
-    6.times { |i| expect(arr[i]) == i }
-  end
-
-  it "dynamic array grows" do
-
+  it "grows a dynamic array" do
+    arr = DynamicArray.new(2)
+    3.times { |i| arr.push(i) }
+    # validate array elements
+    3.times { |i| expect(arr[i]).to eq(i) }
+    5.times { |i| arr.push(i) }
+    # print out array
+    8.times { |i| puts arr[i] }
   end
 end
