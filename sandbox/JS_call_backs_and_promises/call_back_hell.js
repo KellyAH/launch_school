e276a23349e0004482f799ee4e3c3892b9363046
Kelly Hong <k_ahong@yahoo.com>
2021-11-17 19:07:13 -0800

    add js call back exercises

---
+++ sandbox/JS_call_backs_and_promises/call_back_hell.js
@@ -0,0 +1,43 @@
+/* CALLBACKS
+multi nested callbacks -> callback hell
+
+* async execution call chaining callbacks
+
+* not easily readable
+* not easy to debug
+*  fix with promises or async await
+*/
+
+function asyncTask(callback) {
+  console.log('in asyncTask');
+  // setTimeout() method sets a timer which executes a function
+  // or specified piece of code once the timer expires.
+  setTimeout(() => {
+    console.log('in set timeout call back for asyncTask');
+    callback(null, "This is data from server");
+  }, 500);
+}
+
+function makeApiCall(callback) {
+  console.log('in makeApiCall');
+  setTimeout(() => {
+    console.log("This is async task execution")
+    callback();
+  }, 500)
+}
+
+makeApiCall(() => {
+  console.log('4');
+  makeApiCall(() => {
+    console.log('3');
+    asyncTask(() => {
+      console.log('2');
+      asyncTask( () => {
+        console.log('1');
+        asyncTask(() => {
+          console.log('0');
+        })
+      })
+    })
+  })
+})
