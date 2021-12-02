6fc9a2d51dc13fa5edb1b8cf73527bf328134676
Kelly Hong <k_ahong@yahoo.com>
2021-12-01 19:08:25 -0800

    add promise exercises

--- sandbox/javascript/promises/promise.js
+++ sandbox/javascript/promises/promise.js
@@ -1,7 +1,9 @@
 // promises are special objects used to help with async operations
 // avoid callback hell
 // write async code in synconized fashion
-// refcc using async await
+
+// TODO async await - syntactic sugar that uses promises,
+// used to write async code that looks syncronous
 
 // An async function is a function declared with the async keyword, and the await keyword is permitted within them. The async and await keywords enable asynchronous, promise-based behavior to be written in a cleaner style, avoiding the need to explicitly configure promise chains.
 
@@ -9,6 +11,10 @@
 
 const promise = new Promise(function(resolve,reject){
   resolve('resolved promise');
+// make it randomly resolve or reject after a delay
 });
 
-promise.then(result => console.log(result));
+promise
+  .then(result => console.log(result))
+  .catch(result => console.log(result));
+
